9d4c59e5fd9bf5542504e33e91672a11b4956bc7
Keith Yip <kiwi4boy@gmail.com>
2013-11-20 04:41:06 -0500

    added bootstrap and calendar

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
+gem 'bootstrap-sass', '2.3.2.0'
 
 # Use sqlite3 as the database for Active Record
 gem 'pg'
@@ -47,3 +48,5 @@ end
 group :production do
   gem 'rails_12factor'
 end
+
+gem 'fullcalendar-rails'
---
+++ app/assets/javascripts/fullcalendar_init.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+        header: {
+        	left: 'prev',
+        	center: 'title',
+        	right: 'next'
+        },
+        events: 'https://www.google.com/calendar/feeds/epipg7tms366vrmm2t2l69i7lk%40group.calendar.google.com/public/basic'
+    })
+
+});
