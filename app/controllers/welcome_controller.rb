a1447c5ae7219e264ee7f4c940a18b1dc5f7b5ee
Keith Yip <kiwi4boy@gmail.com>
2013-11-20 13:19:34 -0500

    renamed posts to welcome

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def about
+  end
+
+  def home
+  end
+
+  def goals
+  end
+
+  def looking_forward
+  end
+
+  def schedule
+  end
+
+  def contact
+  end
+end
